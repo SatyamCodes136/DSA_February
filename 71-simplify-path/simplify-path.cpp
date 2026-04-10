49ba6b5548384afe3024233567f451587e501a41
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-04-11 02:46:47 +0530

    Time: 3 ms (64.39%) | Memory: 15.2 MB (20.53%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        string curr = "";
+        for(int i = 0; i <= path.size(); i++){
+            if(i == path.size() || path[i] == '/'){
+                if(curr == "" || curr == "."); // ignore
+                else if(curr == ".."){
+                    if(!st.empty()) st.pop();
+                }
+                else st.push(curr);
+                curr = "";
+            }
+            else curr += path[i];
+        }
+        string res = "";
+        while(!st.empty()){
+            res = '/' + st.top() + res;
+            st.pop();
+        }
+        return res.empty() ? "/" : res;
+    }
+};
