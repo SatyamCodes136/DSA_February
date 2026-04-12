202ce4b94a21150aca1a5752bd08b581a2304d2d
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-04-12 15:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (92.92%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-
-    vector<int> CalculatePreMax(vector<int>& height){
-        vector<int> PreMax(height.size());
-        PreMax[0] = height[0];
-        for(int i = 1; i < height.size(); i++){
-            PreMax[i] = max(PreMax[i-1], height[i]);
-        }
-        return PreMax;
-    }
-
-    vector<int> CalculateSuffMax(vector<int>& height){
-        int n = height.size();
-        vector<int> SuffMax(n);
-        SuffMax[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--){
-            SuffMax[i] = max(SuffMax[i+1], height[i]);
-        }
-        return SuffMax;
-    }
-
     int trap(vector<int>& height) {
-        int total = 0;
         int n = height.size();
-        vector<int> PreMax = CalculatePreMax(height);
-        vector<int> SuffMax = CalculateSuffMax(height);
-        for(int i = 0; i < n; i++){
-            if(height[i] < PreMax[i] && height[i] < SuffMax[i]){
-                total += min(PreMax[i], SuffMax[i]) - height[i];
+        int lmax = 0, rmax = 0, total = 0,  l = 0, r = n-1;
+        while(l <= r){
+            if(lmax <= rmax){
+                if(height[l] < lmax){
+                    total += lmax-height[l];
+                }
+                else lmax = height[l];
+                l++;
+            }
+            else {
+                if(height[r] < rmax){
+                    total += rmax-height[r];
+                }
+                else rmax = height[r];
+                r--;
             }
         }
         return total;
