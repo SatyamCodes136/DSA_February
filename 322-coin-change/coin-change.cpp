55f51be04cfc5cede2c4daa433c3086f7b7bbc95
SatyamJais136 <167278982+SatyamJais136@users.noreply.github.com>
2026-04-22 18:32:41 +0530

    Time: 231 ms (5.03%) | Memory: 94 MB (5.04%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int rec(int i, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        if(amount == 0) return 0; // base case
-        if(i >= coins.size() || amount < 0) return 1e9;
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int take = 1 + rec(i, coins, amount-coins[i], dp);
-        int notTake = rec(i+1, coins, amount, dp);
-        return dp[i][amount] = min(take, notTake);
+
+    int rec(vector<int> &coins, vector<vector<int>> &dp, int amount, int i){
+        int n = coins.size();
+        if(i == n){
+            if(amount == 0) return 0;
+            else return INT_MAX;
+        }
+        if(dp[amount][i] != -1) return dp[amount][i];
+        int notTake = rec(coins, dp, amount, i+1);
+        int take = INT_MAX;
+        if(coins[i] <= amount){
+            int res = rec(coins, dp, amount-coins[i], i);
+            if(res != INT_MAX) take = 1 + res;
+        }
+        return dp[amount][i] = min(take, notTake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int res =  rec(0, coins, amount, dp);
-        return res >= 1e9 ? -1 : res;
+        vector<vector<int>> dp(amount+1, vector<int>(n, -1));
+        int x = rec(coins, dp, amount, 0);
+        return x == INT_MAX ? -1 : x;
     }
 };
