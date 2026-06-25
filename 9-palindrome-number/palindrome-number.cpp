750d919727f6c77ddec8f0cd612e4978ee1aabad
Satyam Jaiswal <satyamcodes136@gmail.com>
2026-06-25 11:52:24 +0530

    Time: 3 ms (45.32%) | Memory: 10.9 MB (13.71%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int n = s.size();
+        int i = 0;
+        int j = n-1;
+        while(i < j){
+            if(s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+};
